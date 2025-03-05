08c2cf5c989f23ed2c47a7870ddafc1fb9a03953
Bulelani Gabonewe <bulelanigabonewe@gmail.com>
2025-03-05 12:00:49 +0200

    Add basic functionality

---
+++ scripts/calculator.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelector('.buttons');
+const result = document.querySelector('.result');
+const expression = document.querySelector('.expression');
+
+
+function clear(){
+    expression.textContent = '';
+    result.textContent = '';
+}
+
+
+function addToExpression(value){
+    expression.textContent += value;
+}
+
+
+function evaluateExpression(){
+    console.log("Fuction to be done");
+}
+
+function backSpace(){
+    expression.textContent = expression.textContent.slice(0, -1);
+}
+
+
+buttons.addEventListener('click', (event) => {
+
+    switch(event.target.id){
+        case 'AC':
+            clear();
+            break
+        case '=':
+            evaluateExpression();
+            break;
+        case 'backspace':
+            backSpace();
+            break;
+        default:
+            addToExpression(event.target.id);
+    }
+});
