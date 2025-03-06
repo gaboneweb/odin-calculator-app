f137f9e1dc69df0fa631406da4f6658ba9f9a510
Bulelani Gabonewe <bulelanigabonewe@gmail.com>
2025-03-06 16:23:07 +0200

    Add basic math functions

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,26 @@
 const buttons = document.querySelector('.buttons');
 const expression = document.querySelector('.expression');
 
+function add(firstNumber, secondNumber){
+      return firstNumber + secondNumber;
+}
+
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+
+
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
+}
+
 
 function clear(){
     expression.textContent = '';
