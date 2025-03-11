d246eb3f6747171c28948d70658ed607e4df9d70
Bulelani Gabonewe <bulelanigabonewe@gmail.com>
2025-03-11 19:42:18 +0200

    Make only two numbers in the expression as required

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,5 +1,15 @@
 const buttons = document.querySelector('.buttons');
 const expression = document.querySelector('.expression');
+const decimal = document.getElementById(".");
+
+//Initialise the values
+let firstValue = '';
+let secondValue = '';
+let operator = '';
+
+//Boolean to keep track whether its a new evaluation or not
+let isNewEvaluation = true;
+
 
 function add(firstNumber, secondNumber){
       return firstNumber + secondNumber;
@@ -24,7 +34,12 @@ function divide(firstNumber, secondNumber){
 
 function clear(){
     expression.textContent = '';
+    firstValue = '';
+    secondValue = '';
+    operator = '';
+    decimal.disabled = false;
 }
+
 
 
 function addToExpression(value){
@@ -32,11 +47,35 @@ function addToExpression(value){
 }
 
 
-function evaluateExpression(){
-    console.log("Fuction to be done");
+function evaluateExpression(num1, num2 , operator){
+    switch(operator){
+        case '+':
+            return add(num1,num2);
+        case '÷':
+            return divide(num1, num2);
+        case '-':
+            return subtract(num1,num2);
+        case 'x':
+            return multiply(num1,num2);
+    }
+}
+
+
+function parseNumber(num){
+    if(num.includes("%")){
+        return evaluateExpression(parseFloat(num.replace("%","")), 100, '÷');;
+    }else if(num.includes('.')){
+        return parseFloat(num);
+    }else{
+        return parseInt(num);
+    }
 }
 
 function backSpace(){
+    let currentValue = expression.textContent;
+    if(currentValue !== ''  && currentValue[currentValue.length- 1] === '.'){
+        decimal.disabled = false;
+    }
     expression.textContent = expression.textContent.slice(0, -1);
 }
 
@@ -48,12 +87,34 @@ buttons.addEventListener('click', (event) => {
             clear();
             break
         case '=':
-            evaluateExpression();
+            secondValue = expression.textContent;
+            decimal.disabled = false;
+            expression.textContent = evaluateExpression(parseNumber(firstValue),parseNumber(secondValue), operator);
+            firstValue = expression.textContent;
             break;
         case 'backspace':
             backSpace();
             break;
-        default:
+        case '.':
+            console.log(expression.textContent);
+            if(expression.textContent !== ''){
+                decimal.disabled = true;
+                addToExpression(event.target.id);
+            }
+            break;
+        case '%':
             addToExpression(event.target.id);
+            expression.textContent = parseNumber(expression.textContent);
+            break;
+        default:
+            if(event.target.className.includes('number')){
+                addToExpression(event.target.id);
+                
+            }else{
+                operator = event.target.id;
+                firstValue = expression.textContent;
+                decimal.disabled = false;
+                expression.textContent = '';
+            } 
     }
 });
